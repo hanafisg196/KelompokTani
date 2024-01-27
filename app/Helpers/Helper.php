4104b4a47119d2474c29de6efa67f34fe928b9ac
hanafisg196 <hanafisg196@gmail.com>
2024-01-28 02:30:11 +0700

    fix bug Unsupported operand types: string / int

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -12,9 +12,9 @@ class Helper
             $og_length = $length;
             $last_number = '';
         }else{
-            $code = substr($data->$trow, strlen($prefix)+1);
-            $actial_last_number = ($code/1)*1;
-            $increment_last_number = ((int)$actial_last_number)+1;
+            $code = substr($data->$trow, strlen($prefix) + 1);
+           $actial_last_number = ((int)$code / 1) * 1;
+            $increment_last_number = ((int)$actial_last_number) + 1;
             $last_number_length = strlen($increment_last_number);
             $og_length = $length - $last_number_length;
             $last_number = $increment_last_number;
